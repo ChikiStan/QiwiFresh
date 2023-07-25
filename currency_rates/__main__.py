a96c208d748ad13fc061f21a928f0fd3fd365871
Андрей Чикин <chikin.andrey99@gmail.com>
2023-07-25 14:38:07 +0300

    initial commit

--- currency_rates/__main__.py
+++ currency_rates/__main__.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python
 import datetime
 import logging
 import sys
 
 from functions import Convertor, View, create_parser
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
-                        format="%(message)s")
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
     parser = create_parser()
     namespace = parser.parse_args(sys.argv[1:])
     if namespace.date is None:
-        logging.error('Не указана дата')
+        logging.error("Не указана дата")
         exit(code=-1)
-    date = datetime.datetime.strptime(namespace.date, '%Y-%m-%d').date()
+    date = datetime.datetime.strptime(namespace.date, "%Y-%m-%d").date()
     date = date.strftime("%d/%m/%Y")
     code = namespace.code
     request = Convertor(date=date)
